f7c7403290e0282eb347aed96cf71c6718ce379e
Jan David <jandavid@awesometechnology.de>
2015-11-23 19:53:09 +0100

    Create repository
    A new repository has been created to hold the code for the
    AIGames::Parser gem.

---
+++ lib/ai_games/parser.rb
@@ -0,0 +1,7 @@
+require "ai_games/parser/version"
+
+module AiGames
+  module Parser
+    # Your code goes here...
+  end
+end
---
+++ lib/ai_games/parser/version.rb
@@ -0,0 +1,5 @@
+module AiGames
+  module Parser
+    VERSION = "0.1.0"
+  end
+end
